a618ebc7c84afcaddaeee4c9e0fc3966f5d05e87
Muhammad Shoaib Khan <shoaibmuhammad960@msn.com>
2017-11-08 14:36:11 +0500

    added the project files.

---
+++ footer.php
@@ -0,0 +1,7 @@
+ <!-- Footer -->
+ <footer class="py-5 bg-dark" id="foot">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; <?php echo $site_title; ?> <?php echo date('Y'); ?></p>
+      </div>
+      <!-- /.container -->
+    </footer>
---
+++ variables.php
@@ -0,0 +1,6 @@
+<?php
+
+$site_title = "Cat grooming inc.";
+$site_subtitle = "Styling Cat's since 2004";
+$site_address = "stylingcat.com";
+?>
